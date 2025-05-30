d8bd1d992eccfd2d1f6fc081c94374b2a1d70275
Cameron Russell <camfruss@gmail.com>
2025-05-30 01:34:42 -0400

    Initial files

---
+++ README.md
@@ -0,0 +1,68 @@
+
+## Overview
+
+`boxd` 
+
+bin packing problem NP-Hard significant business implications
+
+aggressive pruning, early termination, multithreading 
+minimize cost with fixed N box types
+
+## Prerequisites
+
+- Safari
+- Develop > Developer Settings... > Allow remote automation
+
+## Usage
+
+## TODO
+
+- [ ] data post-processing / clean-up / add filter args / sort dims
+- [ ] C++ script
+    - [ ] feasibility matrix
+    - [ ] price calculation
+
+```
+// need to take transpose of this for faster computation ... 
+|        | item 1 | item 2 | item 3 | ... | total |
+|--------|--------|--------|--------|-----|-------|
+| box 1  |   0    |   1    |   0    |  .  |   Σ   |
+| box 2  |   1    |   1    |   0    |  .  |   Σ   |
+| box 3  |   1    |   1    |   1    |  .  |   Σ   |
+| ...    |   .    |   .    |   .    | ... |   Σ   |
+| total  |   Σ    |   Σ    |   Σ    |  Σ  |       |
+```
+
+Simplifying Assumptions
+- The minimum order quantity is 1 (vs. 25)
+- For a given dimension X
+    - if X <= 12, X mod 2 = 0 
+    - if X > 12, X mod 4 = 0
+- If an item can be shipped in an AxBxC box, it can also be shipped in a CxBxA box
+    - sometimes boxes open from the side vs. top, resulting in a different dimension order
+
+Invariants
+- boxes are sorted $ -> $$$
+
+Strategy
+- keep track of location ... use long long so can restart if stopped
+- brute force w/ small search space w/ threads
+- generate subsets of size N (via indices ... use unsigned short)
+- take any(item & subset)    TAKE | of all columns
+     - if 0: terminate
+- take MSB index, price++
+- sum all prices
+- keep priority queue of min 1000 prices
+- std::array<uint8_fast_t, N> combination; float cost;
+
+
+start with all at leftmost position
+move rightmost to end, keep repeating until would need to move leftmost down 1
+each thread's leftmost starts at different value ...
+
+N set to 3 to start
+
+## Alternative Strategies
+
+- K-Means clustering
+
---
+++ src/data.py
@@ -0,0 +1,121 @@
+from time import sleep
+
+from bs4 import BeautifulSoup
+import polars as pl
+import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+def load_csv() -> pl.DataFrame:
+    df = pl.read_csv('./data/uline_boxes.csv')
+    return df
+
+def load_page() -> str:
+    url = 'https://www.uline.com/Product/GuidedNav?t=184360'
+
+    driver = webdriver.Safari()
+    driver.get(url)
+
+    height = lambda: driver.execute_script('return document.body.scrollHeight')
+
+    body = driver.find_element(By.TAG_NAME, 'html')
+    while True:
+        h0 = height()
+        body.send_keys(Keys.END)
+        sleep(3)
+
+        if height() <= h0:
+            break
+
+    element = driver.find_element(By.ID, 'tblChartBody')
+    html = f"{element.get_attribute('outerHTML')}"
+    
+    driver.quit()
+    return html
+
+def parse_html(html: str) -> None:
+    rows = []
+    soup = BeautifulSoup(html, 'html.parser')
+    for box in soup.find_all('tr'):
+        try:
+            box_id = box.find('a').get_text(strip=True)  # type: ignore
+            cols = [ col.get_text(strip=True) for col in box.find_all('td')[1:] ]  # type: ignore
+            rows.append({
+                "id": box_id,
+                **{f'col_{i+1}': col for i, col in enumerate(cols)}
+            })
+        except:
+            continue
+
+    df = pl.DataFrame(rows)
+    df.write_csv('./data/uline_boxes.csv')
+
+def transform_df(df: pl.DataFrame) -> pl.DataFrame:
+    result = \
+        df.filter(
+            pl.col('col_2') == '200 lb. Test'
+        ).with_columns(
+            (
+                pl.col('col_1')
+                .map_elements(
+                    lambda s: re.sub(
+                        r'(\d+)⁄(\d+)',
+                        lambda m: f"{str(round(int(m.group(1)) / int(m.group(2)), 1))[1:]}",
+                        s
+                    ),
+                    return_dtype=pl.String
+                )
+                .str.replace_all(r'[" ]', '')
+                .str.split('x')
+                .list.eval(pl.element().str.strip_chars().cast(pl.Float64))
+                .list.sort()
+                .list.to_struct('max_width', fields=['dim_1', 'dim_2', 'dim_3'])
+            ),
+            pl.col('col_2'),
+            pl.col('col_3').str.replace_all(r'[^0-9.]', '').cast(pl.Float64),
+            pl.col('col_4').str.replace_all(r'[^0-9.]', '').cast(pl.Float64),
+            pl.col('col_5').str.replace_all(r'[^0-9.]', '').cast(pl.Float64),
+            pl.col('col_6').str.replace_all(r'[^0-9.]', '').cast(pl.Float64),
+            pl.col('col_7').str.replace_all(r'[^0-9.]', '').cast(pl.Float64)
+        ) \
+        .unnest('col_1') \
+        .drop(['col_8', 'col_9']) \
+        .rename({
+            'col_2': 'box_type',
+            'col_3': 'price_25',
+            'col_4': 'price_100',
+            'col_5': 'price_250',
+            'col_6': 'price_500',
+            'col_7': 'price_1000'
+        })
+    return result
+
+def filter_df(df: pl.DataFrame) -> pl.DataFrame:
+    result = \
+        df.filter(
+            (   
+                pl.col('dim_3') <= 42
+            ),
+            (
+                (pl.col('dim_1') % 2 == 0) & 
+                (pl.col('dim_2') % 2 == 0) & 
+                (pl.col('dim_3') % 2 == 0)
+             )
+        ) \
+        .sort('price_25') \
+        .unique(['dim_1', 'dim_2', 'dim_3'], keep='first', maintain_order=True)
+    return result
+
+def main():
+    # html = load_page()
+    # parse_html(html)
+    df = load_csv()
+    df = transform_df(df)
+    df = filter_df(df)
+    print(df)
+    df.write_csv('tmp.csv')
+
+if __name__ == "__main__":
+    main()
